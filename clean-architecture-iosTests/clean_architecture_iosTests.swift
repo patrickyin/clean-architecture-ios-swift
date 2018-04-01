c6321e087ac03a300a4ec895595fa9978f8f37a3
Patrick Yin <eetech@gmail.com>
2018-04-01 17:50:51 +1000

    add empty project

---
+++ clean-architecture-iosTests/clean_architecture_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  clean_architecture_iosTests.swift
+//  clean-architecture-iosTests
+//
+//  Created by Patrick Yin on 1/4/18.
+//  Copyright © 2018 Patrick Yin. All rights reserved.
+//
+
+import XCTest
+@testable import clean_architecture_ios
+
+class clean_architecture_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
