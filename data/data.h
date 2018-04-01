56871baa1943c11eede129204df5855d018a6179
Patrick Yin <eetech@gmail.com>
2018-04-01 21:15:49 +1000

    add empty data and domain layers

---
+++ data/data.h
@@ -0,0 +1,19 @@
+//
+//  data.h
+//  data
+//
+//  Created by Patrick Yin on 1/4/18.
+//  Copyright © 2018 Patrick Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for data.
+FOUNDATION_EXPORT double dataVersionNumber;
+
+//! Project version string for data.
+FOUNDATION_EXPORT const unsigned char dataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <data/PublicHeader.h>
+
+
---
+++ domain/domain.h
@@ -0,0 +1,19 @@
+//
+//  domain.h
+//  domain
+//
+//  Created by Patrick Yin on 1/4/18.
+//  Copyright © 2018 Patrick Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for domain.
+FOUNDATION_EXPORT double domainVersionNumber;
+
+//! Project version string for domain.
+FOUNDATION_EXPORT const unsigned char domainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <domain/PublicHeader.h>
+
+
