14dfecef5222eb4ab80e9219c950e3ed7a126a71
Patrick Yin <eetech@gmail.com>
2018-04-02 09:48:41 +1000

    add empty files

---
+++ clean-architecture-ios/CleanViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class CleanViewController<P: CleanPresenter>: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
